06adcc1951b8ab42acb144fe74acbf9b2f791598
mudable <mudableshilpa@gmail.com>
2021-10-19 22:45:13 +0530

    Added:UC1 to calculate length.

--- src/linecomparision/LineComparisionProblem.java
+++ src/linecomparision/LineComparisionProblem.java
@@ -1,7 +1,20 @@
 package linecomparision;
 
+import java.util.Scanner;
+
 public class LineComparisionProblem {
+	static double x1, y1, x2, y2;
+
 	public static void main(String[] args) {
 		System.out.println("Welcome To Line Comparision Computation Program.");
+		System.out.println("enter the values");
+		Scanner sc = new Scanner(System.in);
+		x1 = sc.nextDouble();
+		x2 = sc.nextDouble();
+		y1 = sc.nextDouble();
+		y2 = sc.nextDouble();
+		double line1 = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+		System.out.println("the lest is:" + line1);
 	}
+
 }
