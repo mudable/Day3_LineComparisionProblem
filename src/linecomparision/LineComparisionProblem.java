0e31ddf50e45b20627cd7097ede78d64a3c3ca2e
mudable <mudableshilpa@gmail.com>
2021-10-19 22:41:39 +0530

    Added:Welcome message

---
+++ src/linecomparision/LineComparisionProblem.java
@@ -0,0 +1,7 @@
+package linecomparision;
+
+public class LineComparisionProblem {
+	public static void main(String[] args) {
+		System.out.println("Welcome To Line Comparision Computation Program.");
+	}
+}
